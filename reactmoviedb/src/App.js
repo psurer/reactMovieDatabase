4c44d4de4e4fbcec0b9ef6c6d8383a7d2412719e
psurer <surereker@icloud.com>
2022-03-25 21:32:37 +0100

    adding some dummyDate

--- reactmoviedb/src/App.js
+++ reactmoviedb/src/App.js
@@ -1,26 +1,39 @@
 import React from 'react';
-import logo from './logo.svg';
+// import logo from './logo.svg';
 import './App.css';
+import { Fragment } from 'react';
 
 function App() {
+  const dummyMovies = [
+    {
+      id: 1,
+      title: "Some Dummy Movie",
+      openingText: "this is the opening text of the movie",
+      releaseDate: "2020-05-18"
+    },
+    {
+      id: 2,
+      title: "Some Dummy Movie 2",
+      openingText: "this is the opening text of the movie",
+      releaseDate: "2021-05-19"
+    },
+    {
+      id: 3,
+      title: "Some Dummy Movie 3",
+      openingText: "this is the opening text of the movie",
+      releaseDate: "2022-05-20"
+    }
+  ];
+  
   return (
-    <div className="App">
+    <Fragment className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+          Movie DB Search </header>
+          <section>
+            <button>Fetch Movies</button>
+          </section>
+    </Fragment>
   );
-}
-
+} 
+   
 export default App;
--- reactmoviedb/src/index.js
+++ reactmoviedb/src/index.js
@@ -1,14 +1,34 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
+import React from 'react'; // library
+import ReactDOM from 'react-dom'; // library
 import './index.css';
-import App from './App';
+import App from './App'; 
+/* NOTE: these imports are
+  ES6 Syntax so we can import a module
+  and in this case the module is a component */
 import reportWebVitals from './reportWebVitals';
 
+/*
+
+*/
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
+  /* from the React Dom that we import, 
+  we call the render method, and we give it the 
+  component that we want to render, and we tell
+  it where we want to render it
+  */
+  //react stript mode is a good idea, 
+  // it does more checks, so if you do things that are not kosher,
+  // this protects you  
+  <React.StrictMode> 
+    <App />  
   </React.StrictMode>,
-  document.getElementById('root')
+  document.getElementById('root') /* here we tell our application to Render our App 
+  to something called the 'root'
+  if we go under Public Folder, and to Index.html we will see in Index.HTML 
+  we see a <div> with the id='root'
+  it is Inside of this div that we will render 
+  out our complete application
+  */
 );
 
 // If you want to start measuring performance in your app, pass a function
