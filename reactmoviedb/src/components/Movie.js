1efc9d87cf3ac522f0f0f31c86f53ebb4e3aa925
psurer <surereker@icloud.com>
2022-03-25 23:07:41 +0100

    creating movie and movie list components

---
+++ reactmoviedb/src/components/Movie.js
@@ -0,0 +1,15 @@
+import Reaact from 'react';
+
+import classes from './MovieList.module.css';
+
+const Movie = (props) => {
+    return (
+        <li className={classes.movie}>
+        <h2>{props.title}</h2>
+        <h3>{props.releaseDate}</h3>
+        <p>{props.openingText}</p>
+        </li>
+    );
+};
+
+export default Movie;
---
+++ reactmoviedb/src/components/MovieList.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+import classes from './MovieList.module.css';
+
+const MovieList = (props) => {
+    return (
+
+    );
+};
+
+export default MovieList;
